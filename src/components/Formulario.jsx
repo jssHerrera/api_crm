2833059a88d34192ad2a256cc5f96b577af8d93b
Denis <jsherr.6@gmail.com>
2022-01-17 21:57:26 -0500

    cambios en la ruta

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -27,7 +27,7 @@ const Formulario = ({ cliente, cargando }) => {
     try {
       let respuesta
       if (cliente.id) {
-        const url = `http://localhost:4000/clientes/${cliente.id}`;
+        const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`;
         respuesta = await fetch(url, {
           method: "PUT",
           body: JSON.stringify(valores),
@@ -37,7 +37,7 @@ const Formulario = ({ cliente, cargando }) => {
         });
       } else {
         // nuevo registro
-        const url = "http://localhost:4000/clientes";
+        const url = import.meta.env.VITE_API_URL;
         respuesta = await fetch(url, {
           method: "POST",
           body: JSON.stringify(valores),
